6fd67a97e2777c8e1bd8840933de318d28e49d5b
NandorsGames <45879251+NandorsGames@users.noreply.github.com>
2019-10-14 20:18:38 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace V.C.C.A
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(26, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "You:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(26, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Andrea:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.Text = "V.C.C.A";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using AIMLbot;
+
+namespace V.C.C.A
+{
+    public partial class Form1 : Form
+    {
+        private SpeechRecognitionEngine engine;
+        private SpeechSynthesizer synthesizer;
+
+        private Bot bot;
+        private User user;
+
+        private Dictionary<string, string> cmd = new Dictionary<string, string>;
+
+        public void LoadSpeech()
+        {
+            try
+            {
+                #region Speech, Speak, Chatbot
+
+                engine = new SpeechRecognitionEngine();
+                engine.SetInputToDefaultAudioDevice();
+
+                engine.LoadGrammar(new DictationGrammar());
+
+                engine.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(rec);
+
+                engine.RecognizeAsync(RecognizeMode.Multiple);
+
+                bot = new Bot();
+                bot.loadSettings();
+
+                user = new User("Nándor", bot);
+
+                bot.isAcceptingUserInput = false;
+                bot.loadAIMLFromFiles();
+                bot.isAcceptingUserInput = true;
+
+                synthesizer = new SpeechSynthesizer();
+                #endregion
+
+                #region Commands
+                //Command hozzaadashoz parancs utana "," es a kulcs//
+                cmd.Add("What time is it", "WhatTime");
+                cmd.Add("Tell me the time", "WhatTime");
+                cmd.Add("What date is right now", "WhatDate");
+
+                string[] cmds = cmd.Keys.ToArray();
+
+                Choices c_cmds = new Choices(cmds);
+
+                GrammarBuilder gb_cmds = new GrammarBuilder();
+                gb_cmds.Append(c_cmds);
+
+                Grammar g_cmds = new Grammar(gb_cmds);
+                g_cmds.Name = "cmds";
+
+                engine.LoadGrammar(g_cmds);
+                #endregion
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadSpeech();
+
+            if (label2.Text == "Andrea")
+            {
+                synthesizer.SelectVoiceByHints(VoiceGender.Female);
+            }
+        }
+
+        private void rec(object s, SpeechRecognizedEventArgs e)
+        {
+            string speech = e.Result.Text;
+            string answer = string.Empty;
+
+            if(e.Result.Confidence > 0.4f)
+            {
+                label1.Text = "You: " + speech;
+
+                switch (e.Result.Grammar.Name)
+                {
+                    case "cmds":
+                        try
+                        {
+                            string cmdType = cmd[speech];
+                            answer = ProcessCMD(cmdType);
+                        }
+                        catch
+                        {
+                            break;
+                        }
+                        break;
+                    default:
+                        answer = GetResponse(speech);
+                        
+                        break;
+                }
+                label2.Text = "Andrea: " + answer;
+                synthesizer.SpeakAsync(answer);
+            }
+        }
+
+        private string ProcessCMD(string cmdType)
+        {
+            string answer = string.Empty;
+            switch (cmdType)
+            {
+                //Ide kell hozzaadni mindegyik parancsot mint "case"//
+                case "WhatTime":
+                    answer = DateTime.Now.ToShortTimeString();
+                    break;
+
+                case "WhatDate":
+                    answer = DateTime.Now.ToShortDateString();
+                    break;
+            }
+            return answer;
+        }
+
+        private string GetResponse(string query)
+        {
+            Request request = new Request(query, user, bot);
+            Result result = bot.Chat(request);
+            return result.Output;
+        }
+    }
+}
